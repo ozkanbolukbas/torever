ab84f12629f1d78a9bbfcbd043fec4a5351ec417
Özkan Bölükbaş <ozkanbolukbas@gmail.com>
2018-05-28 17:38:46 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,57 @@
+require('./config/config');
+const _ = require('lodash');
+const express = require('express');
+const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
+
+var{mongoose} =require('./db/mongoose');
+var{User} = require('./models/user');
+var{authenticate} = require('./middleware/authenticate');
+
+const app = express();
+const port = process.env.PORT;
+
+app.use(bodyParser.json());
+
+app.post('/users', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+  var user = new User(body);
+
+  user.save().then(()=>{
+    return user.generateAuthToken();
+  }).then((token)=>{
+    res.header("x-auth", token).send(user);
+  }).catch((e)=>{
+    res.status(400).send(e);
+  })
+});
+
+app.get('/users/me', authenticate, (req, res) => {
+  res.send(req.user);
+});
+
+app.post('/users/login', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+
+  User.findByCredentials(body.email, body.password).then((user) => {
+    return user.generateAuthToken().then((token) => {
+      res.header('x-auth', token).send(user);
+    });
+  }).catch((e) => {
+    res.status(400).send();
+  });
+});
+
+app.delete('/users/me/token', authenticate, (req, res) => {
+  req.user.removeToken(req.token).then(() => {
+    res.status(200).send();
+  }, () => {
+    res.status(400).send();
+  });
+});
+
+app.listen(port,()=> {
+  console.log(`Server Starts on ${port}`);
+});
+
+module.exports = {app};
